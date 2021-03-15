96786e0c3b7ce9e72e28c49c4533642f06dccee1
jakubLabedowicz <jalabedowicz@gmail.com>
2021-03-15 13:25:56 +0100

    edit

---
+++ js/Arrow.js
@@ -0,0 +1,9 @@
+function arrow() {
+  document.getElementById('main').style.animation = 'move 2s 1';
+  setTimeout("window.open('https://jakublabendowicz.github.io/projects/FreestyleRandomizer/random.html', '_self')", 1000);
+}
+
+function refresh() {
+  // document.getElementById('container').style.animation = 'move 2s 1';
+  window.open('https://jakublabendowicz.github.io/projects/FreestyleRandomizer/random.html', '_self');
+}
